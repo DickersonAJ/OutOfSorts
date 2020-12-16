7b7c0082f73f352421adb73f9c1f6c28a6d52f39
DickersonAJ <>
2020-12-16 12:17:39 -0500

    Doesn't fully sort yet, but the switching works

--- Sorts.java
+++ Sorts.java
@@ -1,7 +1,24 @@
 public class Sorts{
 
-  public void bubbleSort(int[] data){
-    
+  public static void bubbleSort(int[] data){
+    int temp = 0;
+    boolean swap = false;
+    for (int i = 0; i < data.length - 1; i++) {
+
+      if (data[i] > data[i+1]) {
+        swap = true;
+        temp = data[i];
+        data[i] = data[i+1];
+        data[i+1] = temp;
+      }
+
+      if (swap == false) {
+        i = data.length;
+      }
+      swap = false;
+
+    }
+
   }
 
 }
--- tester.java
+++ tester.java
@@ -1,7 +1,15 @@
+import java.util.Arrays;
 public class tester{
 
   public static void main(String[] args){
-
+    int[] nums = new int[5];
+    nums[0] = 4;
+    nums[1] = 3;
+    nums[2] = 5;
+    nums[3] = 1;
+    nums[4] = 2;
+    Sorts.bubbleSort(nums);
+    System.out.println(Arrays.toString(nums));
   }
 
 }
